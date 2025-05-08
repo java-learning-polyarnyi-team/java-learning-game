fdc41126f5c48261e508d79abcea31e0e3a32259
anechitaev <43794600+anechitaev@users.noreply.github.com>
2025-05-08 15:18:56 +0300

    little help

--- app/src/main/java/ru/learning/Game.java
+++ app/src/main/java/ru/learning/Game.java
@@ -1,6 +1,21 @@
 package ru.learning;
 
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
+        while (true) {
+            Integer input = readConsoleInput();
+            if (input == 1) {
+                return;
+            }
+        }
+    }
+
+    public static Integer readConsoleInput(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Для выхода введите 1: ");
+        Integer inputInt = input.nextInt(); // здесь идет считывание текста
+        return inputInt;
     }
 }
---
+++ app/src/main/java/ru/learning/character/Player.java
@@ -0,0 +1,8 @@
+package ru.learning.character;
+
+public class Player {
+    String name;
+    Integer health;
+    Integer maxHealth;
+    Integer attackPower;
+}
