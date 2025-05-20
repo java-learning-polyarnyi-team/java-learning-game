a9c6de749b17b77e7fd9656af2d411c342b45f4a
Pavlov-Andrey-V <pavlov.and.v@mail.ru>
2025-05-20 18:56:16 +0300

    Добавил описание выбора при вводе значения 1 или 2

--- app/src/main/java/ru/learning/Game.java
+++ app/src/main/java/ru/learning/Game.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
         while (true) {
+            writeOptions();
             Integer input = readConsoleInput();
+            if (input == 1) {
+                System.out.println("Ты выбрал следующий цикл, Меченый");
+            }
             if (input == 2) {
+                System.out.println("ВсеГо хороШеГо");
                 return;
             }
         }
@@ -14,9 +19,12 @@ public class Game {
 
     public static Integer readConsoleInput(){
         Scanner input = new Scanner(System.in);
-        System.out.println("Для повтора введите 1: ");
-        System.out.println("Для выхода введите 2: ");
         Integer inputInt = input.nextInt(); // здесь идет считывание текста
         return inputInt;
+    }
+
+    public static void writeOptions() {
+        System.out.println("Для повтора введите 1: ");
+        System.out.println("Для выхода введите 2: ");
     }
 }
