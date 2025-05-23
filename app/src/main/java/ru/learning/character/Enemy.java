3a38c2d853665ca0fc9d172ad9becb8fd97b16d2
Pavlov-Andrey-V <pavlov.and.v@mail.ru>
2025-05-23 16:47:48 +0300

    Реализовал методы Player и Enemy (#15)
    * Реализовал методы Player и Enemy
    
    * Внес правки в код в методах классов Player и Enemy
    
    * Внес дополнительные правки в код в методах классов Player и Enemy

--- app/src/main/java/ru/learning/character/Enemy.java
+++ app/src/main/java/ru/learning/character/Enemy.java
@@ -1,7 +1,27 @@
 package ru.learning.character;
 
 public class Enemy {
-    String name;
-    Integer health;
-    Integer attackPower;
+    private String name;
+    private Integer health;
+    private Integer attackPower;
+
+    public boolean isAlive() {
+        if (health > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public Integer attack() {
+        attackPower = 10;
+        return attackPower;
+    }
+
+    public void takeDamage(int damage) {
+        if (damage > 0) {
+            health = health - damage;
+        }
+    }
 }
--- app/src/main/java/ru/learning/character/Player.java
+++ app/src/main/java/ru/learning/character/Player.java
@@ -1,8 +1,28 @@
 package ru.learning.character;
 
 public class Player {
-    String name;
-    Integer health;
-    Integer maxHealth;
-    Integer attackPower;
+    private String name;
+    private Integer health;
+    private Integer maxHealth;
+    private Integer attackPower;
+
+    public boolean isAlive() {
+        if (health > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public Integer attack() {
+        attackPower = 10;
+        return attackPower;
+    }
+
+    public void takeDamage(int damage) {
+        if (damage > 0) {
+            health = health - damage;
+        }
+    }
 }
